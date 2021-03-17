519dc8200d1340e2d49489e8c1d57e17fbf2c83d
Jake Taylor <jake@elixxir.io>
2021-03-17 16:09:44 -0500

    fix pipeline

--- game/game_test.go
+++ game/game_test.go
@@ -14,11 +14,11 @@ func TestNew(t *testing.T) {
 
 func TestGame_Play(t *testing.T) {
 	g := New(map[string]uint64{"t1": 0, "t2": 0, "t3": 3}, []byte("salt"), crypto.NewRng())
-	ok, _ := g.Play("addr", "i'm a message")
+	ok, _, _ := g.Play("addr", "i'm a message")
 	if ok {
 		t.Error("Should not have been able to play with unknown address")
 	}
-	ok, _ = g.Play("t1", "i'm not a message")
+	ok, _, _ = g.Play("t1", "i'm not a message")
 	if !ok {
 		t.Error("Failed to play game with address t1")
 	}
